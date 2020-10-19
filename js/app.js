b7e92a7905358d189d6a76c70e16dab212c12438
clintonhays <clintonhays84@gmail.com>
2020-10-19 20:51:37 +0900

    add svgs, add modal styling and functionality

--- js/app.js
+++ js/app.js
@@ -7,12 +7,30 @@
 const menuToggle = document.querySelector('.nav-toggle');
 const navbar = document.querySelector('.navbar');
 const links = document.querySelectorAll('.menulink');
+const openModalButtons = document.querySelectorAll('[data-modal-target]');
+const closeModalButtons = document.querySelectorAll('[data-close-button]');
+const overlay = document.getElementById('overlay');
 
 // Utility Functions
 
 function hamburgerToggle () {
+	// toggles classes to transition navbar
 	document.body.classList.toggle('nav-open');
 	navbar.classList.toggle('navbar-open');
+}
+
+function openModal (modal) {
+	// check if modal exists, add active class to modal & overlay
+	if (modal === null) return;
+	modal.classList.add('active');
+	overlay.classList.add('active');
+}
+
+function closeModal (modal) {
+	// check if modal exists, remove active class to modal & overlay
+	if (modal === null) return;
+	modal.classList.remove('active');
+	overlay.classList.remove('active');
 }
 
 // Event Listeners:
@@ -22,7 +40,29 @@ menuToggle.addEventListener('click', () => {
 });
 
 links.forEach((link) => {
+	// makes navlinks trigger toggle function
 	link.addEventListener('click', () => {
 		hamburgerToggle();
+	});
+});
+
+openModalButtons.forEach((button) => {
+	button.addEventListener('click', () => {
+		const modal = document.querySelector(button.dataset.modalTarget);
+		openModal(modal);
+	});
+});
+
+overlay.addEventListener('click', () => {
+	const modals = document.querySelectorAll('.modal.active');
+	modals.forEach((modal) => {
+		closeModal(modal);
+	});
+});
+
+closeModalButtons.forEach((button) => {
+	button.addEventListener('click', () => {
+		const modal = button.closest('.modal');
+		closeModal(modal);
 	});
 });
