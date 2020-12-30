1d65f3372dd39f0fd65588101f946b939c0282d8
clintonhays <clintonhays84@gmail.com>
2020-12-30 21:40:55 +0900

    update to new design

--- js/app.js
+++ js/app.js
@@ -1,85 +1,11 @@
-// Global Variables:
+const email = document.querySelector(".email-svg");
 
-const menuToggle = document.querySelector('.nav-toggle');
-const navbar = document.querySelector('.navbar');
-const links = document.querySelectorAll('.menulink');
-const openModalButtons = document.querySelectorAll('[data-modal-target]');
-const closeModalButtons = document.querySelectorAll('[data-close-button]');
-const overlay = document.getElementById('overlay');
-const emailButton = document.querySelector('.emailbutton');
-
-// Utility Functions
-
-function hamburgerToggle () {
-	// toggles classes to transition navbar
-	document.body.classList.toggle('nav-open');
-	navbar.classList.toggle('navbar-open');
-}
-
-function openModal (modal) {
-	// check if modal exists, add active class to modal & overlay
-	if (modal === null) return;
-	modal.classList.add('active');
-	overlay.classList.add('active');
-}
-
-function closeModal (modal) {
-	// check if modal exists, remove active class to modal & overlay
-	if (modal === null) return;
-	modal.classList.remove('active');
-	overlay.classList.remove('active');
-}
-
-//
-// - - - - - - - - - - Hamburger Functionality - - - - - - - - - - //
-//
-
-menuToggle.addEventListener('click', () => {
-	hamburgerToggle();
+email.addEventListener("click", (e) => {
+  const emailAddress = email.dataset.clipboardText;
+  navigator.clipboard.writeText(emailAddress);
+  e.target.focus();
 });
 
-links.forEach((link) => {
-	// makes navlinks trigger toggle function
-	link.addEventListener('click', () => {
-		hamburgerToggle();
-	});
-});
-
-//
-// - - - - - - - - - - Modal Functionality - - - - - - - - - - //
-//
-
-openModalButtons.forEach((button) => {
-	// iterate through button array and open matching modal
-	button.addEventListener('click', () => {
-		const modal = document.querySelector(button.dataset.modalTarget);
-		openModal(modal);
-	});
-});
-
-overlay.addEventListener('click', () => {
-	// close overlay
-	const modals = document.querySelectorAll('.modal.active');
-	modals.forEach((modal) => {
-		closeModal(modal);
-	});
-});
-
-closeModalButtons.forEach((button) => {
-	// iterate over array and close open modals
-	button.addEventListener('click', () => {
-		const modal = button.closest('.modal');
-		closeModal(modal);
-	});
-});
-
-//
-// - - - - - - - - - - Copy Email Address - - - - - - - - - - //
-//
-
-// copies email address from "data-clipboard-text" in button
-emailButton.addEventListener('click', (e) => {
-	const email = e.target.dataset.clipboardText;
-	navigator.clipboard.writeText(email);
-	e.target.focus();
+const popover = new bootstrap.Popover(document.querySelector(".email-popover"), {
+  trigger: "focus",
 });
